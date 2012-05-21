2ff76eefa3a64c86e7bad538d4e7fa11da72b5c9
Ngo Duc Hiep <hiepndhut@gmail.com>
2012-05-21 20:04:00 +0700

    cocos2d 2.x support

---
+++ Example/Classes/HelloWorldLayer.h
@@ -0,0 +1,23 @@
+//
+//  HelloWorldLayer.h
+//  CCBladev2
+//
+//  Created by sales on 5/20/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+
+#import <GameKit/GameKit.h>
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
