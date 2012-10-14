1aac6bd7e6cb3e2742fc0d4f29e4c91cd60692cb
Petros Douvantzis <petrakeas@gmail.com>
2012-10-14 22:24:41 +0300

    Popping now occurs in update: and is adjusted according to framerate. Previously slow framerate would result in slowly fading blade. The popping rate can be changed. It is set to 1/60 by default Fixed dealloc bug: using [self clear] the instance would reallocate itself and then check [path count] which would have been deallocated by that time

--- CCBlade.h
+++ CCBlade.h
@@ -9,10 +9,10 @@
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -26,8 +26,9 @@
 #import <Foundation/Foundation.h>
 #import "cocos2d.h"
 
-#define USE_LAGRANGE    1
-#define USE_STL_LIST    0
+#define USE_LAGRANGE        1
+#define USE_STL_LIST        0
+#define USE_UPDATE_FOR_POP  1
 
 inline float fangle(CGPoint vect);
 inline float lagrange1(CGPoint p1, CGPoint p2, float x);
@@ -42,10 +43,13 @@ inline void f1(CGPoint p1, CGPoint p2, float d, CGPoint *o1, CGPoint *o2);
 	CGPoint *vertices;
 	CGPoint *coordinates;
 	BOOL reset;
-	CCTexture2D *_texture;	
+	CCTexture2D *_texture;
 	float width;
     BOOL _finish;
     BOOL _willPop;
+    
+    float timeSinceLastPop;
+    float popTimeInterval;
 }
 @property (readonly)unsigned int pointLimit;
 @property(retain) CCTexture2D *texture;
